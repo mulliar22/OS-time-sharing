d99cb3b1d9296c234f34c6aefb14e27f26a8a566
thederpylama <ianlauver@comcast.net>
2025-03-05 17:53:52 -0500

    Created LoadProcesses function and mock Process class

--- Main.java
+++ Main.java
@@ -1,2 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    //initialize dynamic length array for processes
+    static ArrayList<Process> processes = null;
+
+    public static void main(String[] args) {
+        loadProcesses();
+    }
+
+    public static void loadProcesses() {
+        try {
+            //create new File and Scanner objects
+            File fileObj = new File("processes.txt");
+            Scanner myReader = new Scanner(fileObj);
+
+            while (myReader.hasNextLine()) {
+                // read a line and split it on commas
+                String line = myReader.nextLine();
+                String[] data = line.split(",");
+                System.out.println(data);
+
+                // create new process instances and add them to the list of processes
+                processes.add(new Process(Integer.parseInt(data[0]), Integer.parseInt(data[1]), Integer.parseInt(data[2]), Integer.parseInt(data[3])));
+            }
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- Process.java
+++ Process.java
@@ -1,2 +1,13 @@
 public class Process {
+    int id;
+    int tTime;
+    int eTime;
+    int wTime;
+
+    public Process(int id, int tTime, int eTime, int wTime){
+
+    }
+    public void setID(int id){
+
+    }
 }
